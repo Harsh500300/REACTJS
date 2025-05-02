a008c3787fc5cc67688a3525d334e9e41b952f1a
Harsh Yadav <harsh500300@gmail.com>
2025-05-03 00:36:28 +0530

    All files uploaded to github

---
+++ src/Components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <div className='flex justify-center mt-10 h-80 m-auto w-1/2 bg-red-200 container p-3'>
+            <div className='flex justify-center'>
+            <h1 className='text-bold text-3xl'>TO-DO LIST</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Form
